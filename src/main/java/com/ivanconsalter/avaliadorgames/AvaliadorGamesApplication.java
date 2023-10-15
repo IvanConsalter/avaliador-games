1003b53ddd53110a05ed88d1976cba417d031f95
Ivan Consalter <ivanconsalter1@gmail.com>
2023-10-15 12:33:19 -0300

    project created

---
+++ src/main/java/com/ivanconsalter/avaliadorgames/AvaliadorGamesApplication.java
@@ -0,0 +1,13 @@
+package com.ivanconsalter.avaliadorgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliadorGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliadorGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivanconsalter/avaliadorgames/AvaliadorGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanconsalter.avaliadorgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliadorGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
